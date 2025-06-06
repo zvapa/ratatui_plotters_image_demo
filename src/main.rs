ef6f84cb55ebf25d2c597abfd9e9a5298371e29e
zvapa <eduardvarlam@yahoo.com>
2025-06-06 16:26:47 +0100

    refactor initial state structure

--- src/main.rs
+++ src/main.rs
@@ -11,34 +11,55 @@ use ratatui::{
 use std::io::Error;
 
 #[derive(Default)]
+enum View {
+    Instruments,
+    #[default]
+    List,
+}
+
+#[derive(Default, PartialEq)]
+enum ListMode {
+    #[default]
+    Normal,
+    Insert,
+}
+
+enum FormAction {
+    None,
+    Submit(TodoItem),
+    Escape,
+}
+
+#[derive(Default)]
 struct TodoItem {
     is_done: bool,
     description: String,
 }
 
 #[derive(Default)]
-pub struct AppState {
-    // screen/mode: home
-    items: Vec<TodoItem>,
-    list_state: ListState,
-
-    is_list_add_new: bool,
-    // screen/mode: list add new
-    input_value: String,
-
-    // exit
-    running: bool,
+pub struct State {
+    current_view: View,
+    todo_list: ToDoList,
+    running: bool, // use to exit the app
 }
 
-impl AppState {
+#[derive(Default)]
+pub struct ToDoList {
+    items: Vec<TodoItem>,
+    state: ListState,
+    mode: ListMode,
+    input_value: String,
+}
+
+impl State {
     pub fn init() -> Self {
-        let mut app_state = AppState::default();
-        app_state.running = true;
-        app_state.items.push(TodoItem {
+        let mut state = State::default();
+        state.running = true;
+        state.todo_list.items.push(TodoItem {
             is_done: false,
             description: " add new items here ".to_string(),
         });
-        app_state
+        state
     }
 }
 
@@ -55,47 +76,43 @@ impl AppState {
  */
 
 async fn run(terminal: &mut DefaultTerminal) -> Result<()> {
-    let mut app_state = AppState::init();
+    let mut state = State::init();
     let mut event_stream = EventStream::new();
     loop {
-        let mut f = event_stream.next().fuse();
+        let mut fut = event_stream.next().fuse();
         select! {
-            maybe_event = f => handle_event(maybe_event, &mut app_state).await?,
+            maybe_event = fut => handle_event(maybe_event, &mut state).await?,
         }
-        if !app_state.running {
+        if !state.running {
             break;
         }
-        terminal.draw(|f| render(f, &mut app_state))?;
+        terminal.draw(|f| render(f, &mut state))?;
     }
     Ok(())
 }
 
-async fn handle_event(
-    maybe_event: Option<Result<Event, Error>>,
-    app_state: &mut AppState,
-) -> Result<()> {
+async fn handle_event(maybe_event: Option<Result<Event, Error>>, state: &mut State) -> Result<()> {
     match maybe_event {
         Some(Ok(ev)) => {
             if let Event::Key(key_event) = ev {
                 if key_event.kind == KeyEventKind::Press {
-                    if app_state.is_list_add_new {
-                        match handle_action_list_add_new(app_state, key_event) {
-                            FormAction::None => {}
-                            FormAction::Submit => {
-                                app_state.is_list_add_new = false;
-                                app_state.items.push(TodoItem {
-                                    is_done: false,
-                                    description: app_state.input_value.to_string(),
-                                });
-                                app_state.input_value.clear();
-                            }
-                            FormAction::Escape => {
-                                app_state.is_list_add_new = false;
-                                app_state.input_value.clear();
+                    match state.current_view {
+                        View::Instruments => {}
+                        View::List if state.todo_list.mode == ListMode::Insert => {
+                            match handle_action_list_add_new(state, key_event) {
+                                FormAction::None => {}
+                                FormAction::Submit(item) => {
+                                    state.todo_list.items.push(item);
+                                    state.todo_list.input_value.clear();
+                                    state.todo_list.mode = ListMode::Normal;
+                                }
+                                FormAction::Escape => {
+                                    state.todo_list.mode = ListMode::Normal;
+                                    state.todo_list.input_value.clear();
+                                }
                             }
                         }
-                    } else {
-                        handle_action_home_screen(app_state, key_event);
+                        View::List => handle_action_home_screen(state, key_event),
                     }
                 }
             }
@@ -108,53 +125,52 @@ async fn handle_event(
     Ok(())
 }
 
-enum FormAction {
-    None,
-    Submit,
-    Escape,
-}
-
 // another 'mode' ("list add new item")
-fn handle_action_list_add_new(app_state: &mut AppState, key_event: KeyEvent) -> FormAction {
+fn handle_action_list_add_new(app_state: &mut State, key_event: KeyEvent) -> FormAction {
     match key_event.code {
         KeyCode::Char(c) => {
-            app_state.input_value.push(c);
+            app_state.todo_list.input_value.push(c);
         }
         KeyCode::Backspace => {
-            app_state.input_value.pop();
+            app_state.todo_list.input_value.pop();
         }
         KeyCode::Esc => return FormAction::Escape,
-        KeyCode::Enter => return FormAction::Submit,
+        KeyCode::Enter => {
+            return FormAction::Submit(TodoItem {
+                is_done: false,
+                description: app_state.todo_list.input_value.to_string(),
+            });
+        }
         _ => {}
     }
     FormAction::None
 }
 
 // in 'home' screen (mode)
-fn handle_action_home_screen(app_state: &mut AppState, key_event: KeyEvent) {
+fn handle_action_home_screen(app_state: &mut State, key_event: KeyEvent) {
     match key_event.code {
         KeyCode::Char('q') => {
             app_state.running = false;
         }
         KeyCode::Down => {
-            app_state.list_state.select_next();
+            app_state.todo_list.state.select_next();
         }
         KeyCode::Up => {
-            app_state.list_state.select_previous();
+            app_state.todo_list.state.select_previous();
         }
         KeyCode::Insert => {
-            app_state.is_list_add_new = true;
+            app_state.todo_list.mode = ListMode::Insert;
         }
         KeyCode::Char(' ') => {
-            if let Some(selected_index) = app_state.list_state.selected() {
-                if let Some(td_item) = app_state.items.get_mut(selected_index) {
+            if let Some(selected_index) = app_state.todo_list.state.selected() {
+                if let Some(td_item) = app_state.todo_list.items.get_mut(selected_index) {
                     td_item.is_done = !td_item.is_done; // toggle
                 };
             };
         }
         KeyCode::Delete => {
-            if let Some(selected_index) = app_state.list_state.selected() {
-                app_state.items.remove(selected_index);
+            if let Some(selected_index) = app_state.todo_list.state.selected() {
+                app_state.todo_list.items.remove(selected_index);
             };
         }
         _ => {}
@@ -163,11 +179,11 @@ fn handle_action_home_screen(app_state: &mut AppState, key_event: KeyEvent) {
 
 /// called via ```terminal.draw(|f| render(f, &mut app_state))?;``` line inside [`fn@crate::run`] loop
 /// to render the entire frame based on the current [`struct@crate::AppState`]
-fn render(f: &mut Frame, app_state: &mut AppState) {
+fn render(f: &mut Frame, app_state: &mut State) {
     let [my_area]: [Rect; 1] = Layout::vertical([Constraint::Fill(1)]).areas(f.area());
 
     // add new list item 'mode'
-    if app_state.is_list_add_new {
+    if app_state.todo_list.mode == ListMode::Insert {
         render_list_add_new(f, app_state, my_area);
     } else {
         // 'normal' / 'home' mode
@@ -175,9 +191,9 @@ fn render(f: &mut Frame, app_state: &mut AppState) {
     }
 }
 
-fn render_normal_mode(f: &mut Frame<'_>, app_state: &mut AppState, my_area: Rect) {
+fn render_normal_mode(f: &mut Frame<'_>, app_state: &mut State, my_area: Rect) {
     f.render_stateful_widget(
-        List::new(app_state.items.iter().map(|i| {
+        List::new(app_state.todo_list.items.iter().map(|i| {
             match i.is_done {
                 false => ListItem::new(i.description.trim()).style(Color::LightGreen),
                 true => ListItem::new(i.description.trim()).style(
@@ -196,13 +212,13 @@ fn render_normal_mode(f: &mut Frame<'_>, app_state: &mut AppState, my_area: Rect
                 .title_alignment(Alignment::Center),
         ),
         my_area,
-        &mut app_state.list_state,
+        &mut app_state.todo_list.state,
     )
 }
 
-fn render_list_add_new(f: &mut Frame<'_>, app_state: &mut AppState, my_area: Rect) {
+fn render_list_add_new(f: &mut Frame<'_>, app_state: &mut State, my_area: Rect) {
     f.render_widget(
-        Paragraph::new(app_state.input_value.to_string())
+        Paragraph::new(app_state.todo_list.input_value.to_string())
             .wrap(Wrap { trim: true })
             .block(
                 Block::bordered()
